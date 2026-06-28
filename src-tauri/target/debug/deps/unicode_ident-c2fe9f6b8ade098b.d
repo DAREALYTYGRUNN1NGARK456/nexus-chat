c3df19c035a22a5383a3de061df049474c06c86a
DAREALYTYGRUNN1NGARK456 <andrewt.yd@gmail.com>
2026-06-27 20:35:35 -0400

    Add .gitignore, remove node_modules from tracking

--- src-tauri/target/debug/deps/unicode_ident-c2fe9f6b8ade098b.d
+++
@@ -1,8 +0,0 @@
-C:\Users\andre\Desktop\Coding\SSH\nexus-chat-tauri\src-tauri\target\debug\deps\unicode_ident-c2fe9f6b8ade098b.d: C:\Users\andre\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs C:\Users\andre\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs
-
-C:\Users\andre\Desktop\Coding\SSH\nexus-chat-tauri\src-tauri\target\debug\deps\libunicode_ident-c2fe9f6b8ade098b.rlib: C:\Users\andre\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs C:\Users\andre\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs
-
-C:\Users\andre\Desktop\Coding\SSH\nexus-chat-tauri\src-tauri\target\debug\deps\libunicode_ident-c2fe9f6b8ade098b.rmeta: C:\Users\andre\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs C:\Users\andre\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs
-
-C:\Users\andre\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs:
-C:\Users\andre\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs:
